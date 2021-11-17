4837cf2d6c78649ec4cdf98a648b4790a2358ea4
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2021-11-17 22:56:51 +0530

    Add Env Vars

--- bots/__init__.py
+++ bots/__init__.py
@@ -5,12 +5,15 @@ from tgEasy.config import config
 SupportGroup = config("SupportGroup", default="@DivideProjectsDiscussion")
 JoinChannel = config("JoinChannel", default="@DivideProjects")
 JoinCheck = config("JoinCheck", default=True)
+BotToken = config("BotToken", default="2126891045:AAETy6VIZSStCLbIuKd2TcLOs1qJVoR_Y9w")
+ApiId = config("ApiId", default=1615152)
+ApiHash = config("ApiHash", default="dc9a964e4f31331859dc7d4df007e8d5")
 
 client = Client(
     "bots",
-    api_id=1615152,
-    api_hash="dc9a964e4f31331859dc7d4df007e8d5",
-    bot_token="2126891045:AAETy6VIZSStCLbIuKd2TcLOs1qJVoR_Y9w",
+    api_id=ApiId,
+    api_hash=ApiHash,
+    bot_token=BotToken,
     plugins={"root": "bots/modules"},
 )
 client.set_parse_mode("html")
