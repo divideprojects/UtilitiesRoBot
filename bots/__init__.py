b4ce5a6cb8097a937462a774900bd3bd90527082
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2021-12-01 15:57:27 +0530

    Fix JoinCheck

--- bots/__init__.py
+++ bots/__init__.py
@@ -6,7 +6,9 @@ from tgEasy.config import config
 
 SupportGroup = config("SupportGroup", default="@DivideProjectsDiscussion")
 JoinChannel = config("JoinChannel", default="@DivideProjects")
-JoinCheck = config("JoinCheck", default=True)
+JoinCheck = (
+    True if config("JoinCheck", default=True).lower() in ("true", "yes") else False
+)
 BotToken = config("BotToken", default="2126891045:AAETy6VIZSStCLbIuKd2TcLOs1qJVoR_Y9w")
 ApiId = config("ApiId", default=1615152)
 ApiHash = config("ApiHash", default="dc9a964e4f31331859dc7d4df007e8d5")
