fca0cc1a28ad8754e0817af3d069fe0a1f68c778
Jayant Hegde Kageri <jayantkageri@gmail.com>
2021-12-05 14:23:29 +0530

    bug fix

--- bots/modules/json.py
+++ bots/modules/json.py
@@ -8,7 +8,7 @@ from bots.utils.joinCheck import joinCheck
 @app.command("json", pm_only=True)
 @joinCheck()
 async def json(_, m: Message):
-    msg = m if not m.replied_to_message else m.replied_to_message
+    msg = m if not m.reply_to_message else m.reply_to_message
     if len(length) < 4095:
         return await message.reply_text(str(Code(msg)))
     fName = f"json_{m.from_user.id}_{m.message_id}.json"
