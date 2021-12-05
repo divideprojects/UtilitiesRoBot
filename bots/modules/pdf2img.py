d10f641809bf5a838f48895efb56b55c4ccf1bb9
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2021-12-06 04:55:49 +0530

    Fixed

--- bots/modules/pdf2img.py
+++ bots/modules/pdf2img.py
@@ -30,7 +30,7 @@ from bots.utils.joinCheck import joinCheck
 @joinCheck()
 async def pdf2img(c, m: Message):
     if (
-        m.reply_to_message.document
+        (m.reply_to_message and m.reply_to_message.document)
         and m.reply_to_message.document.mime_type == "application/pdf"
     ):
         user_id = m.from_user.id
