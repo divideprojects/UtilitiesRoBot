c9000b1486e7b180b625e8590b0b978984b132bb
Jayant Hegde Kageri <jayantkageri@gmail.com>
2021-12-05 14:27:13 +0530

    ok

--- bots/modules/json.py
+++ bots/modules/json.py
@@ -9,10 +9,10 @@ from bots.utils.joinCheck import joinCheck
 @joinCheck()
 async def json(_, m: Message):
     msg = m if not m.reply_to_message else m.reply_to_message
-    if len(msg) < 4095:
-        return await message.reply_text(str(Code(msg)))
+    if len(str(msg)) < 4095:
+        return await message.reply_text(str(Code(str(msg))))
     fName = f"json_{m.from_user.id}_{m.message_id}.json"
     with open(fName, "w+") as file:
-        file.write(msg)
+        file.write(str(msg))
         file.close()
     await message.reply_document(fName)
