bf72e9113a19c566c255830e6f90e7fca9498a9b
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2021-12-06 11:09:15 +0530

    Bug fix and optimizations

--- bots/modules/userid.py
+++ bots/modules/userid.py
@@ -60,69 +60,41 @@ async def getid(c: app, message: Message):
         text = f"<b>{use}'s ID:</b> <code>{u.id}</code>\n<b>Chat ID:</b> <code>{message.chat.id}</code>"
         await message.reply_text(text, parse_mode="html")
         return
-    text_unping = "<b>Chat ID:</b>"
     if message.chat.username:
-        text_unping = (
-            f'<a href="https://t.me/{message.chat.username}">{text_unping}</a>'
-        )
-    text_unping += f" <code>{message.chat.id}</code>\n"
-    text = "<b>Message ID:</b>"
+        text_ping = f'<a href="https://t.me/{message.chat.username}"><b>Chat ID:</b></a> <code>{message.chat.id}</code>\n'
+    else:
+        text_ping = f"<b>Chat ID:</b> <code>{message.chat.id}</code>\n"
     if message.link:
-        text = f'<a href="{message.link}">{text}</a>'
-    text += f" <code>{message.message_id}</code>\n"
-    text_unping += text
+        text_ping += f'<a href="{message.link}"><b>Message ID:</b></a> <code>{message.message_id}</code>\n'
+    else:
+        text_ping += f"<b>Message ID:</b> <code>{message.message_id}</code>\n"
     if message.from_user:
-        text_unping += f'<b><a href="tg://user?id={message.from_user.id}">Your ID:</a></b> <code>{message.from_user.id}</code>\n'
-    if message.sender_chat:
-        text_unping += '<b><a href="tg://user?id=1087968824">Your ID:</a></b> <code>1087968824</code>\n'
-    text_ping = text_unping
+        text_ping += f'<b><a href="tg://user?id={message.from_user.id}">Your ID:</a></b> <code>{message.from_user.id}</code>\n'
+    elif message.sender_chat:
+        text_ping += '<b><a href="tg://user?id=1087968824">Your ID:</a></b> <code>1087968824</code>\n'
     reply = message.reply_to_message
     if not getattr(reply, "empty", True):
-        text_unping += "\n"
-        text = "<b>Replied Message ID:</b>"
         if reply.link:
-            text = f'<a href="{reply.link}">{text}</a>'
-        text += f" <code>{reply.message_id}</code>\n"
-        text_unping += text
-        text_ping = text_unping
+            text_ping += f'\n<a href="{reply.link}"><b>Replied Message ID:</b></a> <code>{reply.message_id}</code>\n'
+        else:
+            text_ping += f"\n<b>Replied Message ID:</b> <code>{reply.message_id}</code>\n"
         if message.reply_to_message and message.reply_to_message.forward_from_chat:
-            text_unping += f"\n<b>Forwarded channel ID:</b> <code>{message.reply_to_message.forward_from_chat.id}</code>\n"
-            text_ping = text_unping
+            text_ping += f"\n<b>Forwarded channel ID:</b> <code>{message.reply_to_message.forward_from_chat.id}</code>\n"
         if reply.from_user:
-            tex = "<b>Replied User ID:</b>"
             if reply.from_user.username:
-                text = f'<a href="https://t.me/{reply.from_user.username}">{text}</a>'
-            text += f" <code>{reply.from_user.id}</code>\n"
-            text_unping += text
-            text_ping += f'<b><a href="tg://user?id={reply.from_user.id}">Replied User ID:</a></b> <code>{reply.from_user.id}</code>\n'
-        if message.sender_chat:
-            tex = "<b>Replied User ID:</b>"
-            text = f'<a href="https://t.me/GroupAnonymousBot">{tex}</a>'
-            text += " <code>1087968824</code>\n"
-            text_unping += text
-            text_ping += '<b><a href="tg://user?id=1087968824">Replied User ID:</a></b> <code>1087968824</code>\n'
+                text_ping += f'<a href="https://t.me/{reply.from_user.username}"><b>Replied User ID:</b></a> <code>{reply.from_user.id}</code>\n'
+            else:
+                text_ping += f'<b><a href="tg://user?id={reply.from_user.id}">Replied User ID:</a></b> <code>{reply.from_user.id}</code>\n'
+        elif reply.sender_chat:
+            text_ping += f'<a href="https://t.me/GroupAnonymousBot"><b>Replied User ID:</b></a> <code>1087968824</code>\n'
         if reply.forward_from:
-            text_unping += "\n"
-            text = "<b>Forwarded User ID:</b>"
             if reply.forward_from.username:
-                text = (
-                    f'<a href="https://t.me/{reply.forward_from.username}">{text}</a>'
-                )
-            text += f" <code>{reply.forward_from.id}</code>\n"
-            text_unping += text
-            text_ping += f'\n<b><a href="tg://user?id={reply.forward_from.id}">Forwarded User ID:</a></b> <code>{reply.forward_from.id}</code>\n'
+                text_ping += f'\n<a href="https://t.me/{reply.forward_from.username}"><b>Forwarded User ID:</b></a> <code>{reply.forward_from.id}</code>\n'
+            else:
+                text_ping += f'\n<b><a href="tg://user?id={reply.forward_from.id}">Forwarded User ID:</a></b> <code>{reply.forward_from.id}</code>\n'
         if message.reply_to_message and message.reply_to_message.sticker:
-            text_unping += f"\n<b>Sticker ID:</b> <code>{message.reply_to_message.sticker.file_id}</code>\n\n"
-            text_ping = text_unping
+            text_ping += f"\n<b>Sticker ID:</b> <code>{message.reply_to_message.sticker.file_id}</code>\n\n"
         if message.reply_to_message and message.reply_to_message.animation:
-            text_unping += f"\n<b>GIF ID:</b> <code>{message.reply_to_message.animation.file_id}</code>\n"
-            text_ping = text_unping
-    reply = await message.reply_text(
-        text_unping, disable_web_page_preview=True, parse_mode="html"
-    )
-    if text_unping != text_ping:
-        await reply.edit_text(
-            text_ping, disable_web_page_preview=True, parse_mode="html"
-        )
-        return
+            text_ping += f"\n<b>GIF ID:</b> <code>{message.reply_to_message.animation.file_id}</code>\n"
+    await message.reply_text(text_ping, disable_web_page_preview=True, parse_mode="html")
     return
