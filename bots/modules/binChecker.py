c314f139e60ab29cb8d81f42678f55f4d5f8c42c
Jayant Hegde Kageri <jayantkageri@gmail.com>
2021-12-13 18:03:13 +0530

    fix

--- bots/modules/binChecker.py
+++ bots/modules/binChecker.py
@@ -11,5 +11,5 @@ async def binChecker(_, m: Message):
     msg = await m.reply_text("...")
     if len(m.command) == 0:
         return await msg.edit_text("Please type a bin after the command.")
-    CCBin = m.text.split(None, 1)[1]
+    CCBin = m.command[1]
     await msg.edit_text(await getBinInfo(CCBin))
