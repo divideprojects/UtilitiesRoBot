b5118c69b613dd04a1323d17791646327a848be6
Jayant Hegde Kageri <jayantkageri@gmail.com>
2022-04-03 18:52:29 +0530

    Update vars.py

--- bots/vars.py
+++ bots/vars.py
@@ -8,7 +8,7 @@ class Vars:
     SUPPORT_GROUP = config(
         "SUPPORT_GROUP", default="@DivideProjectsDiscussion")
     JOIN_CHANNEL = config("JOIN_CHANNEL", default="@DivideProjects")
-    JOIN_CHECK = config("JOIN_CHECK", default=None)
+    JOIN_CHECK = bool(config("JOIN_CHECK", default=None))
     BOT_TOKEN = config("BOT_TOKEN", default="")
     API_ID = config("API_ID", default=123456)
     API_HASH = config("API_HASH", default="")
