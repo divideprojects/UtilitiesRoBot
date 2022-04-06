297bf959a5cbbe3119ccfef5dc5176a4bbaab7bd
Jayant Hegde Kageri <jayantkageri@gmail.com>
2022-04-06 17:17:29 +0530

    Update tr.py

--- bots/modules/tr.py
+++ bots/modules/tr.py
@@ -21,13 +21,13 @@ async def translate(client, message):
             return msg.edit_text(usage)
         text = message.reply_to_message.text.markdown
 
-    if message.len == 1:
+    if len(message.command) == 1:
         toLanguage = message.command[1]
         if not message.reply_to_message:
             return msg.edit(usage)
         text = message.reply_to_message.text.markdown
 
-    if message.len == 2:
+    if len(message.command) == 2:
         toLanguage = message.command[1]
         text = message.command[2]
 
