5ff2c4b53c6b20123a5904bccddc09fcde337910
Jayant Hegde Kageri <jayantkageri@gmail.com>
2022-05-15 11:48:08 +0530

    Update binChecker.py

--- bots/modules/binChecker.py
+++ bots/modules/binChecker.py
@@ -2,7 +2,7 @@ from pyrogram.types import Message
 
 from bots import MODULES, app
 
-# from bots.utils.captcha import hcaptcha
+from bots.utils.captcha import hcaptcha
 from bots.utils.getBinInfo import getBinInfo
 from bots.utils.joinCheck import joinCheck
 
@@ -18,7 +18,7 @@ MODULES.update(
 
 @app.command("bin", pm_only=True)
 @joinCheck()
-# @hcaptcha()
+@hcaptcha()
 async def binChecker(_, m: Message):
     msg = await m.reply_text("...")
     if len(m.command) == 1:
